c5420b499836d6a44640f1756f587ff5eefb71f8
sandykdanik17 <danik2007.ds10@gmail.com>
2025-04-29 09:17:38 +0300

    Update task3.cpp
    Змінено код

--- laba_5/task3/task3/task3.cpp
+++ laba_5/task3/task3/task3.cpp
@@ -1,6 +1,9 @@
-﻿#include <iostream>
+#include <iostream>
+#include <fstream>
 #include <string>
 #include <Windows.h>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 class Person {
@@ -22,6 +25,12 @@ public:
     }
 
     virtual ~Person() {}
+
+    
+    void setName(const string& n) { name = n; }
+    void setAge(int a) { age = a; }
+    string getName() const { return name; }
+    int getAge() const { return age; }
 
     friend istream& operator>>(istream& in, Person& p);
     friend ostream& operator<<(ostream& out, const Person& p);
@@ -65,6 +74,12 @@ public:
 
     ~Student() override {}
 
+    
+    void setUniversity(const string& u) { university = u; }
+    void setCourse(int c) { course = c; }
+    string getUniversity() const { return university; }
+    int getCourse() const { return course; }
+
     friend istream& operator>>(istream& in, Student& s);
     friend ostream& operator<<(ostream& out, const Student& s);
 };
@@ -85,14 +100,75 @@ ostream& operator<<(ostream& out, const Student& s) {
     return out;
 }
 
+
+string randomName() {
+    const string names[] = { "Іван", "Марія", "Олег", "Олена", "Андрій" };
+    return names[rand() % 5];
+}
+
+string randomUniversity() {
+    const string universities[] = { "КНУ", "ЛПНУ", "ХНУРЕ", "КПІ", "ЛНУ" };
+    return universities[rand() % 5];
+}
+
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
+    srand(static_cast<unsigned>(time(0)));
 
-    Student s;
-    cout << "--- Введення студента ---\n";
-    cin >> s;
-    cout << "\n--- Введені дані ---\n" << s << endl;
+    Person p1;
+    cout << "Введення вручну для Person\n";
+    cin >> p1;
+    cout << "\nВведені дані:\n" << p1 << endl;
+
+    cout << "\nЗчитування з файлу для Person\n";
+    ifstream finPerson("input_person.txt");
+    if (finPerson.is_open()) {
+        string name;
+        int age;
+        finPerson >> name >> age;
+        p1.setName(name);
+        p1.setAge(age);
+        finPerson.close();
+        cout << "Дані з файлу:\n" << p1 << endl;
+    }
+    else {
+        cout << "Не вдалося відкрити файл input_person.txt!\n";
+    }
+
+    cout << "\nГенерація випадкових даних для Person\n";
+    p1.setName(randomName());
+    p1.setAge(rand() % 60 + 18);
+    cout << p1 << endl;
+
+    Student s1;
+    cout << "\nВведення вручну для Student\n";
+    cin >> s1;
+    cout << "\nВведені дані:\n" << s1 << endl;
+
+    cout << "\nЗчитування з файлу для Student\n";
+    ifstream finStudent("input_student.txt");
+    if (finStudent.is_open()) {
+        string name, university;
+        int age, course;
+        finStudent >> name >> age >> university >> course;
+        s1.setName(name);
+        s1.setAge(age);
+        s1.setUniversity(university);
+        s1.setCourse(course);
+        finStudent.close();
+        cout << "Дані з файлу:\n" << s1 << endl;
+    }
+    else {
+        cout << "Не вдалося відкрити файл input_student.txt!\n";
+    }
+
+    cout << "\nГенерація випадкових даних для Student\n";
+    s1.setName(randomName());
+    s1.setAge(rand() % 60 + 18);
+    s1.setUniversity(randomUniversity());
+    s1.setCourse(rand() % 6 + 1);
+    cout << s1 << endl;
 
     return 0;
 }
